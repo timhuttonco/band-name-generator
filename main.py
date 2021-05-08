c8f95fd65738a003ae47a151221217177b101773
Tim Hutton <>
2021-05-08 11:34:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello and welcome to the band name generator!")
+city = input("What city did you grow up in?\n")
+pet = input("What was the name of your childhood pet?\n")
+print("Your band name is " + city + " " + pet)
